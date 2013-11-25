d394e26fffad146a69fc21367b43c5be6e4e0e5c
SleepyJO <joachimgirschik@hotmail.de>
2013-11-25 22:16:57 +0100

    Aktueller Stand

---
+++ src/de/rType/menu/MenuItemKeys.java
@@ -0,0 +1,11 @@
+package de.rType.menu;
+
+public class MenuItemKeys {
+
+	public static final String START = "start";
+	public static final String NEW_GAME = "newGame";
+	public static final String PAUSE = "pause";
+	public static final String OPTIONS = "options";
+	public static final String EXIT = "exit";
+
+}
